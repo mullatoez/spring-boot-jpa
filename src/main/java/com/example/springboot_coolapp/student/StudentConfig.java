0bb11192581d691f34d38129f1c05d83f3056b70
mullatoez <kimanthikevin76@gmail.com>
2022-01-19 01:41:24 +0300

    Added @PostMapping

--- src/main/java/com/example/springboot_coolapp/student/StudentConfig.java
+++ src/main/java/com/example/springboot_coolapp/student/StudentConfig.java
@@ -19,22 +19,19 @@ public class StudentConfig {
             Student kevin = new Student(
                     "Kevin",
                     "kevin.kyei@gmail.com",
-                    LocalDate.of(2000, AUGUST, 23),
-                    21
+                    LocalDate.of(2000, AUGUST, 23)
             );
 
             Student mirriam = new Student(
                     "Mirriam",
                     "mirriam.kyei@gmail.com",
-                    LocalDate.of(2005, SEPTEMBER, 9),
-                    17
+                    LocalDate.of(2005, SEPTEMBER, 9)
             );
 
             Student milka = new Student(
                     "Milka",
                     "milka.mutiso@yahoo.com",
-                    LocalDate.of(1999, DECEMBER, 27),
-                    22
+                    LocalDate.of(1999, DECEMBER, 27)
             );
 
             repository.saveAll(

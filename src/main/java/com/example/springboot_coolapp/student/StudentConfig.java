a32894f8726c769a819d2387bdd4cc8639d7634e
mullatoez <kimanthikevin76@gmail.com>
2022-01-19 01:27:58 +0300

    Run hibernate to add data to db

---
+++ src/main/java/com/example/springboot_coolapp/student/StudentConfig.java
@@ -0,0 +1,45 @@
+package com.example.springboot_coolapp.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args ->
+        {
+            Student kevin = new Student(
+                    "Kevin",
+                    "kevin.kyei@gmail.com",
+                    LocalDate.of(2000, AUGUST, 23),
+                    21
+            );
+
+            Student mirriam = new Student(
+                    "Mirriam",
+                    "mirriam.kyei@gmail.com",
+                    LocalDate.of(2005, SEPTEMBER, 9),
+                    17
+            );
+
+            Student milka = new Student(
+                    "Milka",
+                    "milka.mutiso@yahoo.com",
+                    LocalDate.of(1999, DECEMBER, 27),
+                    22
+            );
+
+            repository.saveAll(
+                    List.of(kevin, mirriam, milka)
+            );
+        };
+    }
+}
